8e40c5e9da003136711f59681b4eedf976873bab
ZQInQAAS <450472377@qq.com>
2018-03-09 16:56:46 +0800

    add

---
+++ Codes/braindecode/code/FBCSP-Motor.py
@@ -0,0 +1,129 @@
+
+# coding: utf-8
+
+# In[1]:
+
+
+import logging
+# import importlib
+# importlib.reload(logging) # see https://stackoverflow.com/a/21475297/1469195
+log = logging.getLogger()
+log.setLevel('DEBUG')
+import sys
+logging.basicConfig(format='%(asctime)s %(levelname)s : %(message)s',
+                     level=logging.DEBUG, stream=sys.stdout)
+
+import os
+os.sys.path.append('../fbcsp/')
+os.sys.path.append('../braindecode/')
+
+# get_ipython().magic(u'load_ext autoreload')
+# get_ipython().magic(u'autoreload 2')
+# get_ipython().magic(u'cd /home/schirrmr/')
+
+import matplotlib
+# get_ipython().magic(u'matplotlib inline')
+# get_ipython().magic(u"config InlineBackend.figure_format = 'svg'")
+matplotlib.rcParams['figure.figsize'] = (12.0, 1.0)
+matplotlib.rcParams['font.size'] = 7
+
+import logging
+log = logging.getLogger()
+log.setLevel('DEBUG')
+
+
+# In[18]:
+
+
+# from braindecode.datasets.bbci import BBCIDataset
+# set_loader = BBCIDataset('data/BBCI-only-last-runs/BhNoMoSc1S001R13_ds10_1-2BBCI.mat',
+#                         load_sensor_names=['C3', 'C4', 'C1', 'C2', 'CPz', 'F3', 'F4', 'Fz'])
+# cnt = set_loader.load()
+# cnt = cnt.drop_channels(['STI 014'])
+# eog_cnt = BBCIDataset('data/BBCI-only-last-runs/BhNoMoSc1S001R13_ds10_1-2BBCI.mat',
+#                         load_sensor_names=['EOGh', 'EOGv']).load()
+# eog_cnt = eog_cnt.drop_channels(['STI 014'])
+
+from braindecode.datasets.bcic_iv_2a import BCICompetition4Set2A
+data_folder = 'D:\Myfiles\EEGProject\BCICompetitionIV\BCICIV_2a_gdf'
+subject_id = 1 # 1-9
+train_filename = 'A{:02d}T.gdf'.format(subject_id)
+test_filename = 'A{:02d}E.gdf'.format(subject_id)
+train_filepath = os.path.join(data_folder, train_filename)
+test_filepath = os.path.join(data_folder, test_filename)
+train_label_filepath = train_filepath.replace('.gdf', '.mat')
+test_label_filepath = test_filepath.replace('.gdf', '.mat')
+train_loader = BCICompetition4Set2A(train_filepath, labels_filename=train_label_filepath)
+test_loader = BCICompetition4Set2A(test_filepath, labels_filename=test_label_filepath)
+train_cnt = train_loader.load()
+test_cnt = test_loader.load()
+cnt = train_cnt
+# In[19]:
+
+
+from braindecode.mne_ext.signalproc import mne_apply
+from braindecode.datautil.signalproc import bandpass_cnt
+
+import numpy as np
+def car_cnt(cnt):
+    return mne_apply(lambda x: x - np.mean(x,axis=0, keepdims=True), cnt)
+
+cnt = car_cnt(cnt)
+cnt = mne_apply(lambda x: bandpass_cnt(x, 0, 38.0, cnt.info['sfreq']), cnt)
+
+
+
+# In[20]:
+
+
+from fbcsp.experiment import CSPExperiment
+from collections import OrderedDict
+name_to_start_codes = OrderedDict([('left', 1), ('right', 2), ('foot', 3), ('tongue', 4)])
+epoch_ival_ms = [0, 4000]
+
+
+# In[21]:
+
+
+from fbcsp.clean import EOGMaxMinCleaner, VarCleaner, apply_multiple_cleaners
+
+
+# cleaners = [EOGMaxMinCleaner(eog_cnt, epoch_ival_ms, name_to_start_codes, threshold=600),
+#            VarCleaner(whisker_percent=10, whisker_length=3)]
+# cleaned_cnt = apply_multiple_cleaners(cnt, epoch_ival_ms, name_to_start_codes, cleaners)
+
+
+# In[28]:
+
+cleaned_cnt = cnt
+exp = CSPExperiment(cleaned_cnt, name_to_start_codes, epoch_ival_ms,
+                    min_freq = 4,
+                    max_freq=40, last_low_freq=40,
+                    low_width=4,
+                    high_width=4,
+                    low_overlap=0,
+                    high_overlap=0,
+                    filt_order=3,
+                    n_folds=10,
+                    n_top_bottom_csp_filters=5,
+                    n_selected_filterbands=None,
+                    # n_selected_features=20,
+                    forward_steps=2,
+                    backward_steps=1,)
+
+
+# In[29]:
+
+
+from braindecode.datautil.trial_segment import create_signal_target, create_signal_target_from_raw_mne
+
+from braindecode.datautil.signalproc import bandpass_cnt
+
+from braindecode.datautil.iterators import get_balanced_batches
+
+
+# In[30]:
+
+
+exp.run()
+
---
+++ Codes/p/loadov.py
@@ -0,0 +1,98 @@
+# -*- coding: utf-8 -*-
+import scipy.io as sio
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn import preprocessing
+
+def loadov(path):
+
+    # data = sio.loadmat(r'D:\Myfiles\openvibefiles\MI-CSP-r1\signals\GH\GH-171225-acquisition-2.mat')
+    data = sio.loadmat(path)
+    Fs = data['samplingFreq']  # 采样 500Hz
+    data_x = data['samples']  # 数据
+    data_x_time = data['sampleTime']
+    data_x_time = data_x_time[:, 0].tolist()
+    stims = data['stims']  # 标签[time; ID; duration] (10个769,10个770)
+    channel_names=[]
+    for ch in data['channelNames'][0]:
+        channel_names.append(ch[0])
+    info={}
+    info['channal']=channel_names
+    info['rate']=Fs
+
+    # 确定 769/770 cue 出现位置
+    label = []
+    label_time= []
+    trialsize = 0
+    for i in range(stims.shape[0]):
+        if stims[i, 1] == 769 or stims[i, 1] == 770:
+            trialsize = trialsize + 1
+            label.append(stims[i, 1]-769)
+            label_time.append(stims[i, 0])
+    pos = []
+    for i in range(len(label_time)):
+        for j in range(len(data_x_time)):
+            if data_x_time[j] > label_time[i]:
+                pos.append(j)
+                break
+
+
+    width = 1000
+    overlap = 0.75
+    length = 1500
+    window_num = int((length - width) / ((1 - overlap) * width) + 1)
+    j = 0
+    signal_label = []
+    channal_num = data_x.shape[1]
+    signal3d = np.zeros([width, channal_num, trialsize * window_num])
+    for k in range(len(label)):
+        for i in range(window_num):
+            signal_label.append(label[k])
+            start = int(pos[k] + i * ((1 - overlap) * width)) + 500
+            signal3d[:, :, j] = data_x[start:start + width, :]
+            j = j + 1
+
+    # data_x = reduceModerate(signal3d, signal_label)
+    data_x = normalizeDatax(signal3d)
+
+    # t = np.linspace(1, data_x.shape[0], data_x.shape[0])
+    #plt.figure(1)
+    #plt.subplot(211)
+    # plt.plot(t[0:400], train_x[0:400, 3, 2])
+    #plt.plot(t, data_x[:, 2, 30])
+    #plt.subplot(212)
+    # plt.plot(t[0:400], data_x[0:400, 3, 2])
+    #plt.plot(t, data_x[:, 3, 30])
+    #plt.show()
+
+    return data_x, signal_label, info
+
+
+def reduceModerate(signal3d, signal_label):
+    # 减静息态均值
+    data_x = np.zeros(signal3d.shape)
+    samples = signal3d.shape[0]
+    sum = np.zeros(signal3d.shape[1])
+    for i in range(len(signal_label)):
+        if signal_label[i] == 1:
+            for j in range(samples):
+                sum = sum + signal3d[j, :, i]
+            mean = sum / samples
+            break
+        else:
+            continue
+    for i in range(len(signal_label)):
+        for j in range(samples):
+            data_x[j, :, i] = signal3d[j, :, i] - np.transpose(mean)
+    return data_x
+
+def normalizeDatax(signal3d):
+    # 归一化
+    data_x = np.zeros(signal3d.shape)
+    channal_num = signal3d.shape[1]
+    for i in range(signal3d.shape[2]):
+        for j in range(channal_num):
+            data_x[:, j, i] = preprocessing.scale(signal3d[:, j, i])
+    return data_x
+
+
